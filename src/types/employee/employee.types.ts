c1530ced34b0b5a3245476a888a5dd35d4d9cfef
890191443 <safavi.somaye@golrang.com>
2023-02-26 15:01:31 +0330

    add folder and service and types

---
+++ src/types/employee/employee.types.ts
@@ -0,0 +1,14 @@
+// GIG_GEN_Employee
+export type TEmployee = {
+  Id: string
+  CCompanyId: number
+  CEmployeeNumber: number
+  CEmail: string
+  CEmployeeStatusCode: number
+  CNationalityNumber: string
+  CWorkPlaceName: string
+  FullName: string
+  EmployeeId: number
+  WorkPlaceId: number
+  ID: number
+}
