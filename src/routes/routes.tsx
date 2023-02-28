d487b8606d86f2911a2a757cef48b0df537b4400
seyedebrahimi.masoud <seyedebrahimi.masoud@golrang.com>
2023-02-28 11:12:20 +0330

    add router 'add-suggestion'

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,5 +1,6 @@
 import { Main } from 'layout/main/Main';
 import { TestStructure } from 'pages/test-structure';
+import { AddSuggestion } from 'pages/add-suggestion';
 import { createBrowserRouter } from 'react-router-dom';
 
 export const router = createBrowserRouter([
@@ -12,6 +13,10 @@ export const router = createBrowserRouter([
         path: 'test-structure',
         element: <TestStructure />,
       },
+      {
+        path: 'add-Suggestion',
+        element: <AddSuggestion />
+      }
     ],
   },
 ]);
