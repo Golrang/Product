bb98db53591888e958df60a64993a6f922cb6968
seyedebrahimi.masoud <seyedebrahimi.masoud@golrang.com>
2023-02-28 11:10:44 +0330

    add component recommender

--- src/pages/add-suggestion/AddSuggestion.tsx
+++ src/pages/add-suggestion/AddSuggestion.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Form } from 'sharepoint-golrang-design-system';
+import { Recommender } from './containers/recommender'
 
-function AddSuggestion() {
-  return <>add-suggestion</>;
+export const AddSuggestion = () => {
+  const submit = () => {
+  }
+  return <>
+    <Form name='recommenderForm' onSubmit={submit}>
+      <Recommender />
+    </Form>
+  </>;
 }
 
-export default AddSuggestion;
