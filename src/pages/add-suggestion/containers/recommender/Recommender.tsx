d7a9bec2594b42f7975efad5c1288af9124cb66b
seyedebrahimi.masoud <seyedebrahimi.masoud@golrang.com>
2023-03-01 13:33:13 +0330

    change userInfo objName

--- src/pages/add-suggestion/containers/recommender/Recommender.tsx
+++ src/pages/add-suggestion/containers/recommender/Recommender.tsx
@@ -1,22 +1,32 @@
-import { FormInput } from 'sharepoint-golrang-design-system'
-import { getUserInfo } from '../../../../services/general/user-info/userInfo.service'
+import { FormInput } from 'sharepoint-golrang-design-system';
+import { getUserInfo } from '../../../../services/general/user-info/userInfo.service';
 
-const { userInfo } = getUserInfo()
+const { userInfo } = getUserInfo();
 
 const lable = [
   { lable: 'نام و نام خانوادگی', name: userInfo.userFarsiName },
-  { lable: 'سمت', name:  userInfo.PRole },
-  { lable: 'واحد سازمانی', name: userInfo.DName },
-  { lable: 'نام شرکت', name: userInfo.companyName }]
+  { lable: 'سمت', name: userInfo.position },
+  { lable: 'واحد سازمانی', name: userInfo.departman },
+  { lable: 'نام شرکت', name: userInfo.companyName },
+];
 
 export const Recommender = () => {
   return (
     <>
-      <div className='orderer'>
+      <div className="orderer">
         {lable.map((item) => {
-          return <FormInput label={item.lable} name={item.name} key={item.lable} disabled defaultValue={item.name} style={{ color: 'black' }} />
+          return (
+            <FormInput
+              label={item.lable}
+              name={item.name}
+              key={item.lable}
+              disabled
+              defaultValue={item.name}
+              style={{ color: 'black' }}
+            />
+          );
         })}
       </div>
     </>
   );
-}
+};
