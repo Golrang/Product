0d815106b1e1cc71d886f58a3b9fb92169f32d0e
Niloofar <Khademi.Niloufar@Golrang.com>
2023-03-05 09:20:17 +0330

    set TherapeuticField part for add suggestion form

--- src/pages/add-suggestion/containers/therapeutic-field/TherapeuticField.tsx
+++ src/pages/add-suggestion/containers/therapeutic-field/TherapeuticField.tsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import { FormTextArea, FormRadio } from 'sharepoint-golrang-design-system';
+import { Col, Row } from 'antd';
+import { useGetTherapeuticField } from '../../hooks/useGetTherapeuticField';
 
-function TherapeuticField() {
-  return <>TherapeuticField</>;
-}
+export const TherapeuticField = () => {
+  const { data } = useGetTherapeuticField();
+  console.log(data);
+
+  if (data && data !== undefined)
+    return (
+      <Row gutter={24}>
+        <Col md={24} sm={24}>
+          <FormRadio name="TherapeuticFieldId" options={data} />
+          <FormTextArea
+            name="TherapeuticFieldComment"
+            label="توضیحات"
+            placeholder="توضیحات حوزه درمانی"
+          />
+        </Col>
+      </Row>
+    );
+  return null;
+};
 
 export default TherapeuticField;
