90a7dade85db01b171bd7e0e270aa969d3317021
Niloofar <Khademi.Niloufar@Golrang.com>
2023-03-05 10:06:13 +0330

    prettier added

--- src/pages/add-suggestion/AddSuggestion.tsx
+++ src/pages/add-suggestion/AddSuggestion.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
-import {Form, FormInput} from "sharepoint-golrang-design-system"
-import { Col, Row } from 'antd'
+import { Form, FormInput } from 'sharepoint-golrang-design-system';
+import { Col, Row } from 'antd';
 
 function AddSuggestion() {
-  return (<>
-  <Form name="AddSuggestionForm">
-    <Row gutter={24}>
-      <Col md={12} sm={24}>
-        <FormInput name="Material" type="text" label="نام ماده یا مواد موثره" placeholder="ماده موثره اصلی"/>
-      </Col>
-    </Row>
-  </Form>
-  </>);
+  return (
+    <>
+      <Form name="AddSuggestionForm">
+        <Row gutter={24}>
+          <Col md={12} sm={24}>
+            <FormInput
+              name="Material"
+              type="text"
+              label="نام ماده یا مواد موثره"
+              placeholder="ماده موثره اصلی"
+            />
+          </Col>
+        </Row>
+      </Form>
+    </>
+  );
 }
 
 export default AddSuggestion;
