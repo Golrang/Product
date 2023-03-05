a8d4d3ecc36d7f943330d13ff91d062f09419e1b
Niloofar <Khademi.Niloufar@Golrang.com>
2023-03-05 13:06:23 +0330

    put TherapeuticFieldRadio component in TherapeuticField

--- src/pages/add-suggestion/containers/therapeutic-field/TherapeuticField.tsx
+++ src/pages/add-suggestion/containers/therapeutic-field/TherapeuticField.tsx
@@ -1,25 +1,17 @@
-import { FormTextArea, FormRadio } from 'sharepoint-golrang-design-system';
-import { Col, Row } from 'antd';
-import { useGetTherapeuticField } from '../../hooks/useGetTherapeuticField';
+import { FormTextArea } from 'sharepoint-golrang-design-system';
+import { TherapeuticFieldRadio } from './TherapeuticFieldRadio';
 
 export const TherapeuticField = () => {
-  const { data } = useGetTherapeuticField();
-  console.log(data);
-
-  if (data && data !== undefined)
-    return (
-      <Row gutter={24}>
-        <Col md={24} sm={24}>
-          <FormRadio name="TherapeuticFieldId" options={data} />
-          <FormTextArea
-            name="TherapeuticFieldComment"
-            label="توضیحات"
-            placeholder="توضیحات حوزه درمانی"
-          />
-        </Col>
-      </Row>
-    );
-  return null;
+  return (
+    <>
+      <TherapeuticFieldRadio />
+      <FormTextArea
+        name="TherapeuticFieldComment"
+        label="توضیحات"
+        placeholder="توضیحات حوزه درمانی"
+      />
+    </>
+  );
 };
 
 export default TherapeuticField;
