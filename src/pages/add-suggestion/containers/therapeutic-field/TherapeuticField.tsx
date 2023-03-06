ee7c7527c5603085b6a7d58bf2e0f0ebc66ba725
sherinpoor.zahra <Zahra.Sherinpoor@Golrang.com>
2023-03-06 14:15:49 +0330

    complete TherapeuticField component

--- src/pages/add-suggestion/containers/therapeutic-field/TherapeuticField.tsx
+++ src/pages/add-suggestion/containers/therapeutic-field/TherapeuticField.tsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import { FormSelect, FormTextArea } from 'components';
+import { useGetTherapeuticField } from 'pages/add-suggestion/hooks/useGetTherapeuticField';
+import { TSuggestion } from 'types/suggestion/suggestion.types'
 
-function TherapeuticField() {
-  return <>TherapeuticField</>;
-}
+export const TherapeuticField = () => {
+  const { allTherapeuticField } = useGetTherapeuticField();
 
-export default TherapeuticField;
+  type TKeyOfForm = keyof TSuggestion
+  return (
+    <>
+      <FormSelect<TKeyOfForm>
+        name="TherapeuticFieldId"
+        label="حوزه درمانی"
+        showSearch
+        options={allTherapeuticField}
+        filterOption={(input: any, option: any) =>
+          (option?.label ?? '').includes(input)
+        }
+      />
+
+      <FormTextArea<TKeyOfForm>
+        label="توضیحات"
+        name="TherapeuticFieldComment"
+        placeholder="توضیحات حوزه درمانی"
+        autoSize={{ minRows: 4 }}
+      />
+    </>
+  );
+};
