4c93fe3a37ca89061f868fb6fcdd06d5393a4505
Niloofar <Khademi.Niloufar@Golrang.com>
2023-03-11 10:23:26 +0330

    Folder structure

--- src/pages/add-suggestion/containers/material/Material.tsx
+++ src/pages/add-suggestion/containers/material/Material.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
 import { useFieldArray } from 'react-hook-form';
 import { Col, Row, Collapse } from 'antd';
 import { Button, FormInput } from 'components';
-import { ICDelete, ICPlus } from 'icons'
+import { ICDelete, ICPlus } from 'icons';
 
 export const Material = () => {
   const { fields, append, remove } = useFieldArray({
@@ -15,7 +14,11 @@ export const Material = () => {
     <>
       <Collapse>
         <Panel header="اجزاء ثانویه" key="1">
-          <Button className='!inline-flex !items-center btn btn-danger' htmlType="button" onClick={() => append({})}>
+          <Button
+            className="!inline-flex !items-center btn btn-danger"
+            htmlType="button"
+            onClick={() => append({})}
+          >
             <ICPlus className="w-4" />
           </Button>
 
@@ -30,7 +33,11 @@ export const Material = () => {
               </Col>
 
               <Col md={2}>
-                <Button className='!inline-flex !items-center btn btn-danger' htmlType="button" onClick={() => remove(index)}>
+                <Button
+                  className="!inline-flex !items-center btn btn-danger"
+                  htmlType="button"
+                  onClick={() => remove(index)}
+                >
                   <ICDelete className="w-4" />
                 </Button>
               </Col>
--- src/pages/add-suggestion/containers/questions/Questions.tsx
+++ src/pages/add-suggestion/containers/questions/Questions.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
-import { ConsumableQuestion } from './ConsumableQuestion';
-import { PharmaceuticalFormsQuestion } from './PharmaceuticalFormsQuestion';
-import { TherapeuticFieldQuestion } from './TherapeuticFieldQuestion';
+import { ConsumableQuestion } from '../consumable-question/ConsumableQuestion';
+import { PharmaceuticalFormsQuestion } from '../pharmaceutical-forms-question/PharmaceuticalFormsQuestion';
+import { TherapeuticFieldQuestion } from '../therapeutic-field-question/TherapeuticFieldQuestion';
 
 export const Questions = () => {
   return (
