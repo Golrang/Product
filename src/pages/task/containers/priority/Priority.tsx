7c00c2d452515255b089cbde8e62ee4ddac4720c
seyedebrahimi.masoud <seyedebrahimi.masoud@golrang.com>
2023-03-11 15:58:39 +0330

    add Priority Components

--- src/pages/task/containers/priority/Priority.tsx
+++ src/pages/task/containers/priority/Priority.tsx
@@ -1,3 +1,27 @@
+import { Col, Row } from 'antd';
+import { FormSelect, Form } from 'components';
+import { TKeyOfForm } from 'types/priority/priority.types';
+import { useGetPriority } from '../../hooks/useGetPriority';
 export const Priority = () => {
-    return <div>Priority</div>
-}
+  const { allPriority } = useGetPriority();
+  return (
+    <>
+      <Form name="FormPriority" onSubmit={() => console.log('asd')}>
+        <Row gutter={24}>
+          <Col md={12} sm={12}>
+            <FormSelect<TKeyOfForm>
+              name="Title"
+              label="اولویت"
+              mode="multiple"
+              showSearch
+              options={allPriority}
+              filterOption={(input, option) =>
+                (option?.label ?? '').toString().includes(input)
+              }
+            />
+          </Col>
+        </Row>
+      </Form>
+    </>
+  );
+};
---
+++ src/services/priority/priority.service.ts
@@ -0,0 +1,11 @@
+export const getPriority = () => {
+  const data = [
+    { Id: 1, Title: 'p1' },
+    { Id: 2, Title: 'p2' },
+    { Id: 3, Title: 'p3' },
+    { Id: 4, Title: 'p4' },
+    { Id: 5, Title: 'p5' },
+    { Id: 6, Title: 'p6' },
+  ];
+  return data;
+};
