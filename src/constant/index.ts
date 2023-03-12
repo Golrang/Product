aeffff52b6a4be9a2b37621bb17b692f7897899f
890191443 <safavi.somaye@golrang.com>
2023-03-12 10:33:38 +0330

    change services by new path

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,7 +1,6 @@
 export const listName = {
   pharmaceuticalForm: 'GPI_PS_PharmaceuticalForms',
-  TherapeuticField: 'GPI_PS_TherapeuticField',
   productSuggestion: 'GPI_PS_ProductSuggestion',
-  therapeuticField: 'GPI_PS_TherapeuticField ',
+  therapeuticField: 'GPI_PS_TherapeuticField',
   offerReason: 'GPI_PS_OfferReason',
 };
--- src/services/product-suggestion/allProductSuggestion.service.ts
+++ src/services/product-suggestion/allProductSuggestion.service.ts
@@ -1,17 +1,17 @@
-// import { QueryFunctionContext } from '@tanstack/react-query';
-import { suggestions } from 'services/muck/muckData';
+import { listName } from 'constant';
+import { getListItemByFilter } from 'services/general/pnp/pnpjs';
 import { TSuggestion } from 'types/suggestion/suggestion.types';
 
 export const getAllProductSuggestion =
-  async (): // context: QueryFunctionContext<[string, string]>
+  async ():  //context: QueryFunctionContext<[string, string]>
   Promise<TSuggestion[]> => {
-    // const res = await getListItemByFilter<TSuggestion[]>(
-    //   listName.productSuggestion,
-    //   {
-    //     filter: '',
-    //     select: '*',
-    //     expand: '',
-    //   }
-    // );
-    return suggestions; //refactor to res
+    const res = await getListItemByFilter<TSuggestion[]>(
+      listName.productSuggestion,
+      {
+        filter: '',
+        select: '*',
+        expand: '',
+      }
+    );
+    return  res
   };
