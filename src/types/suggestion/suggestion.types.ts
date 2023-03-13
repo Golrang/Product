57bde96e975930a62f9a146ba8608e25dc8bd601
890191443 <safavi.somaye@golrang.com>
2023-03-13 10:52:47 +0330

    add type and change

--- src/types/suggestion/suggestion.types.ts
+++ src/types/suggestion/suggestion.types.ts
@@ -1,18 +1,37 @@
 export type TSuggestion = {
   Id?: number;
+  Title?: string;
   CompanyId: string;
   EmployeeId: string;
   Material: string; //ماده موثره
   PharmaceuticalFormId: number; //شکل دارویی
   OtherPharmaceuticalFormId: number; //سایر اشکال دارویی
+  PharmaceuticalForm?: {
+    Id?: number;
+    Title: string;
+  };
+  OtherPharmaceuticalForm?: {
+    Id?: number;
+    Title: string;
+  };
   PharmaceuticalForm_Other: string;
   OtherPharmaceuticalForm_Other: string;
   BrandName: string; //برند
   ManufacturerCompanyName: string; // شرکت سازنده
   Consumable: string; //مورد مصرف
   TherapeuticFieldId: number; //حوزه درمانی
+  TherapeuticField?: {
+    Id?: number;
+    Title: string;
+  };
   TherapeuticFieldComment: string;
-  OfferReasonId: number; //دلیل پیشنهاد
+  OfferReasonId: [number]; //دلیل پیشنهاد
+  OfferReason?: [
+    {
+      Id?: number;
+      Title: string;
+    }
+  ];
   OfferReasonComment: string;
   ProductAdvatage: string; //مزیت محصول
   ProductWeaknesses: string; //معایب محصول
@@ -24,4 +43,35 @@ export type TSuggestion = {
   Modified?: string; //تاریخ ایجاد
 };
 
-export type TKeyOfForm = keyof TSuggestion
+export type TMaterial = {
+  Title: string;
+};
+export type TSuggestionForm = Pick<
+  TSuggestion,
+  | 'Title'
+  | 'CompanyId'
+  | 'EmployeeId'
+  | 'PharmaceuticalFormId'
+  | 'OtherPharmaceuticalFormId'
+  | 'PharmaceuticalForm_Other'
+  | 'OtherPharmaceuticalForm_Other'
+  | 'BrandName'
+  | 'ManufacturerCompanyName'
+  | 'Consumable'
+  | 'TherapeuticFieldId'
+  | 'TherapeuticFieldComment'
+  | 'Material'
+  | 'OfferReasonId'
+  | 'OfferReasonComment'
+  | 'ProductAdvatage'
+  | 'ProductWeaknesses'
+  | 'SimilarPharmaceuticalForm'
+  | 'SimilarTherapeuticField'
+  | 'SimilarConsumable'
+  | 'Comment'
+> & {
+  Materials: TMaterial[];
+  File: unknown;
+};
+
+export type TKeyOfForm = keyof TSuggestionForm;
