2a4cf8ef030cd06abbbc9ad6817d2904ada4b3e8
sherinpoor.zahra <Zahra.Sherinpoor@Golrang.com>
2023-04-05 15:17:28 +0330

    add style for view suggestion

--- apps/Product/src/pages/task/containers/view-product-suggestion/ViewProductSuggestion.tsx
+++ apps/Product/src/pages/task/containers/view-product-suggestion/ViewProductSuggestion.tsx
@@ -11,11 +11,16 @@ export const ViewProductSuggestion = () => {
   const onFinish = () => {};
   return (
     <div className="border p-4 shadow shadow-indigo-500/40 hover:shadow-indigo-500/40 rounded-lg">
-      <Form name="ViewSuggestionForm" {...{ onSubmit, defaultValues, onFinish }}>
+      <Form
+        name="ViewSuggestionForm"
+        {...{ onSubmit, defaultValues, onFinish }}
+      >
         <Row gutter={24}>
           <Col md={8} sm={24}>
             <div className="Recommender ">
-              <span className="w-[100%] border-t-2 border-solid border-indigo-200 inline-block mb-5  rounded-lg p-1 text-white bg-indigo-300">پیشنهاد دهنده</span>
+              <span className="w-[100%] border-t-2 border-solid border-indigo-200 inline-block mb-5  rounded-lg p-1 text-white bg-indigo-300">
+                پیشنهاد دهنده
+              </span>
               <Row gutter={24}>
                 <Col md={8} sm={24}>
                   <ICPeople className="border rounded-full w-3/4 mt-10 shadow shadow-indigo-500/40 text-gray-500" />
@@ -24,10 +29,18 @@ export const ViewProductSuggestion = () => {
                 <Col md={16} sm={24}>
                   <div className="mt-6">
                     <Descriptions column={1}>
-                      <Descriptions.Item label="نام و نام خانوادگی">زهرا شرین پور</Descriptions.Item>
-                      <Descriptions.Item label="سمت">توسعه دهنده فرانت</Descriptions.Item>
-                      <Descriptions.Item label="واحد سازمانی">شیرپوینت</Descriptions.Item>
-                      <Descriptions.Item label="نام شرکت">گلرنگ سیستم</Descriptions.Item>
+                      <Descriptions.Item label="نام و نام خانوادگی">
+                        زهرا شرین پور
+                      </Descriptions.Item>
+                      <Descriptions.Item label="سمت">
+                        توسعه دهنده فرانت
+                      </Descriptions.Item>
+                      <Descriptions.Item label="واحد سازمانی">
+                        شیرپوینت
+                      </Descriptions.Item>
+                      <Descriptions.Item label="نام شرکت">
+                        گلرنگ سیستم
+                      </Descriptions.Item>
                     </Descriptions>
                   </div>
                 </Col>
@@ -36,28 +49,76 @@ export const ViewProductSuggestion = () => {
           </Col>
           <Col md={16} sm={24}>
             <div className="ProductInformation">
-              <span className="w-[100%] border-t-2 border-solid border-indigo-200 inline-block mb-5 rounded-lg p-1 text-white bg-indigo-300">مشخصات محصول</span>
+              <span className="w-[100%] border-t-2 border-solid border-indigo-200 inline-block mb-5 rounded-lg p-1 text-white bg-indigo-300">
+                مشخصات محصول
+              </span>
               <Descriptions column={3}>
-                <Descriptions.Item label="نام ماده یا مواد موثره">{defaultValues?.Material}</Descriptions.Item>
-                <Descriptions.Item label="اجزا ثانویه">{defaultValues?.Material}</Descriptions.Item>
-                <Descriptions.Item label="شکل دارویی">{defaultValues?.PharmaceuticalForm_Other}</Descriptions.Item>
-                <Descriptions.Item label="سایر اشکال دارویی">{defaultValues?.PharmaceuticalForm?.Title}</Descriptions.Item>
-                <Descriptions.Item label="نام برند اصلی">{defaultValues?.BrandName}</Descriptions.Item>
-                <Descriptions.Item label="نام شرکت سازنده">{defaultValues?.ManufacturerCompanyName}</Descriptions.Item>
+                <Descriptions.Item label="نام ماده یا مواد موثره">
+                  {defaultValues?.Material}
+                </Descriptions.Item>
+                <Descriptions.Item label="اجزا ثانویه">
+                  {defaultValues?.Material}
+                </Descriptions.Item>
+                <Descriptions.Item label="شکل دارویی">
+                  {defaultValues?.PharmaceuticalForm_Other}
+                </Descriptions.Item>
+                <Descriptions.Item label="سایر اشکال دارویی">
+                  {defaultValues?.PharmaceuticalForm?.Title}
+                </Descriptions.Item>
+                <Descriptions.Item label="نام برند اصلی">
+                  {defaultValues?.BrandName}
+                </Descriptions.Item>
+                <Descriptions.Item label="نام شرکت سازنده">
+                  {defaultValues?.ManufacturerCompanyName}
+                </Descriptions.Item>
               </Descriptions>
             </div>
             <Descriptions column={1}>
-              <Descriptions.Item label="مورد مصرف">{defaultValues?.Consumable}</Descriptions.Item>
-              <Descriptions.Item label="حوزه درمانی">{defaultValues?.TherapeuticField?.Title}</Descriptions.Item>
-              <Descriptions.Item label="توضیحات">{defaultValues?.TherapeuticFieldComment}</Descriptions.Item>
-              <Descriptions.Item label="علت پیشنهادی">{defaultValues?.OfferReason?.Title}</Descriptions.Item>
-              <Descriptions.Item label="توضیحات">{defaultValues?.OfferReasonComment}</Descriptions.Item>
-              <Descriptions.Item label="مزیت محصول پیشنهادی نسبت به محصول موجود در بازار">{defaultValues?.ProductAdvatage}</Descriptions.Item>
-              <Descriptions.Item label="نقاط ضعف احتمالی محصول پیشنهادی نسبت به محصول موجود در بازار">{defaultValues?.ProductWeaknesses}</Descriptions.Item>
-              <Descriptions.Item label="آیا محصول با فرم دارویی مشابه توسط شرکت تولید شده است">{defaultValues?.SimilarPharmaceuticalForm ? <ICPlus className="w-6 text-green-600" /> : <ICClose className="w-6 text-red-600" />}</Descriptions.Item>
-              <Descriptions.Item label="آیا محصول با حوزه درمانی مشابه توسط شرکت تولید شده است">{defaultValues?.SimilarTherapeuticField ? <ICPlus className="w-6 text-green-600" /> : <ICClose className="w-6 text-red-600" />}</Descriptions.Item>
-              <Descriptions.Item label="آیا محصول با مورد مصرف مشابه توسط شرکت تولید شده است">{defaultValues?.SimilarConsumable ? <ICPlus className="w-6 text-green-600" /> : <ICClose className="w-6 text-red-600" />}</Descriptions.Item>
-              <Descriptions.Item label="توضیحات">{defaultValues?.Comment}</Descriptions.Item>
+              <Descriptions.Item label="مورد مصرف">
+                {defaultValues?.Consumable}
+              </Descriptions.Item>
+              <Descriptions.Item label="حوزه درمانی">
+                {defaultValues?.TherapeuticField?.Title}
+              </Descriptions.Item>
+              <Descriptions.Item label="توضیحات">
+                {defaultValues?.TherapeuticFieldComment}
+              </Descriptions.Item>
+              <Descriptions.Item label="علت پیشنهادی">
+                {defaultValues?.OfferReason?.Title}
+              </Descriptions.Item>
+              <Descriptions.Item label="توضیحات">
+                {defaultValues?.OfferReasonComment}
+              </Descriptions.Item>
+              <Descriptions.Item label="مزیت محصول پیشنهادی نسبت به محصول موجود در بازار">
+                {defaultValues?.ProductAdvatage}
+              </Descriptions.Item>
+              <Descriptions.Item label="نقاط ضعف احتمالی محصول پیشنهادی نسبت به محصول موجود در بازار">
+                {defaultValues?.ProductWeaknesses}
+              </Descriptions.Item>
+              <Descriptions.Item label="آیا محصول با فرم دارویی مشابه توسط شرکت تولید شده است">
+                {defaultValues?.SimilarPharmaceuticalForm ? (
+                  <ICPlus className="w-6 text-green-600" />
+                ) : (
+                  <ICClose className="w-6 text-red-600" />
+                )}
+              </Descriptions.Item>
+              <Descriptions.Item label="آیا محصول با حوزه درمانی مشابه توسط شرکت تولید شده است">
+                {defaultValues?.SimilarTherapeuticField ? (
+                  <ICPlus className="w-6 text-green-600" />
+                ) : (
+                  <ICClose className="w-6 text-red-600" />
+                )}
+              </Descriptions.Item>
+              <Descriptions.Item label="آیا محصول با مورد مصرف مشابه توسط شرکت تولید شده است">
+                {defaultValues?.SimilarConsumable ? (
+                  <ICPlus className="w-6 text-green-600" />
+                ) : (
+                  <ICClose className="w-6 text-red-600" />
+                )}
+              </Descriptions.Item>
+              <Descriptions.Item label="توضیحات">
+                {defaultValues?.Comment}
+              </Descriptions.Item>
             </Descriptions>
           </Col>
         </Row>
