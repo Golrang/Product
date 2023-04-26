6e657f41ec7428a8765ce3547dd0bda5e38bd3ae
Niloofar <Khademi.Niloufar@Golrang.com>
2023-04-26 10:02:14 +0330

    Main and headerPanel has been changed to new design

--- apps/Product/src/layout/header/HeaderPanel.tsx
+++ apps/Product/src/layout/header/HeaderPanel.tsx
@@ -1,52 +1,22 @@
-import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { ICMenuUp } from "sharepoint-golrang-design-system";
-// import logo from "../../assets/img/gig-logo-small.png";
-export const HeaderPanel = () => {
-  const [collapsed, setCollapsed] = useState(false);
+import "../../NewDesign.css";
 
-  const handelCollapsed = () => setCollapsed(!collapsed);
+export const HeaderPanel = () => {
   return (
     <>
-      <nav className="bg-white py-2 md:py-4">
-        <div className="container px-4 mx-auto md:flex md:items-center">
-          <div
-            className={` md:flex flex-col md:flex-row md:ml-auto mt-3 md:mt-0 !w-[100%] justify-end text-end
-                ${collapsed ? "flex justify-end text-end" : "hidden "}`}
-            dir="ltr"
-            id="navbar-collapse"
-          >
-            <Link
-              to="cartable"
-              className="p-2 lg:px-4 md:mx-2 text-white rounded bg-indigo-600"
-            >
-              کارتابل
-            </Link>
-            <Link
-              to="/"
-              className="p-2 lg:px-4 md:mx-2 text-gray-600 rounded hover:bg-gray-200 hover:text-gray-700 transition-colors duration-300"
-            >
-              گزارش
-            </Link>
-          </div>
-
-          <div className="flex justify-between items-center">
-            {/* <Image
-              width={200}
-              src={logo}
-              preview={false}
-              className="!w-[50px] font-bold text-xl text-indigo-600"
-            /> */}
-            <button
-              className="border border-solid border-gray-600 px-3 py-1 rounded text-gray-600 opacity-50 hover:opacity-75 md:hidden"
-              id="navbar-toggle"
-              onClick={handelCollapsed}
-            >
-              <ICMenuUp className="font-bold text-xl text-indigo-600" />
-            </button>
-          </div>
+      <div className="top-bar">
+        <div className="intro-x breadcrumb ml-auto hidden sm:flex">
+          <Link to="/" className="breadcrumb--active">
+            گزارش
+          </Link>
+          <Link to="cartable" className="breadcrumb--active mr-3">
+            کارتابل
+          </Link>
         </div>
-      </nav>
+        <div className="intro-x relative ml-3 sm:ml-6">
+          <h2 className="text-lg font-medium">سامانه نظام پیشنهادات</h2>
+        </div>
+      </div>
     </>
   );
 };
